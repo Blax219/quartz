4d23d3a24a541d44240d16894447fb10039b4036
Blax219 <165563278+Blax219@users.noreply.github.com>
2024-04-01 00:55:14 +0200

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -20,8 +20,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
-    Component.MobileOnly(Component.Explorer()),
     Component.MobileOnly(Component.PageTitle()),
+    Component.MobileOnly(Component.Spacer()),
+    Component.MobileOnly(Component.Explorer()),
     Component.MobileOnly(Component.Darkmode()),
     Component.DesktopOnly(Component.PageTitle()),
     Component.DesktopOnly(Component.Search()),
